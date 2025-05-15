9179a2015fb0f6ed51ee280e753f1b1af616758e
419425903@qq.com <419425903@qq.com>
2025-05-15 18:00:10 +0800

    添加了一个UI框架

--- shilai2/shilai2/ViewController.swift
+++ shilai2/shilai2/ViewController.swift
@@ -15,10 +15,18 @@ class ViewController: UIViewController {
         print("222")
         print("333")
         // Do any additional setup after loading the view.
+        self.view.addSubview(saveView())
     }
     
-    func saveView(){
+    func saveView() -> UIView{
+        var label = UILabel(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
+        label.text = "1123"
+        label.textColor = UIColor(red: 100, green: 100, blue: 100, alpha: 1.0)
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 19)
         print("保存界面")
+        return label
+    
     }
 
 
